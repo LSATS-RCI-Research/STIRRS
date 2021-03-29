18b279640c05cf469a157974a5e143cf54974dac
Joe Bauer <joebauer@umich.edu>
2021-03-29 14:36:17 -0400

    Added v3 compatible version of Bulk Export from GitLab

---
+++ modules/BulkExport/src/Api/Adapter/ExportAdapter.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+namespace BulkExport\Api\Adapter;
+
+use BulkExport\Api\Representation\ExportRepresentation;
+use BulkExport\Entity\Export;
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\QueryBuilder;
+use Omeka\Api\Adapter\AbstractEntityAdapter;
+use Omeka\Api\Request;
+use Omeka\Entity\EntityInterface;
+use Omeka\Stdlib\ErrorStore;
+
+class ExportAdapter extends AbstractEntityAdapter
+{
+    protected $sortFields = [
+        'id' => 'id',
+        'exporter_id' => 'exporterId',
+    ];
+
+    public function getResourceName()
+    {
+        return 'bulk_exports';
+    }
+
+    public function getRepresentationClass()
+    {
+        return ExportRepresentation::class;
+    }
+
+    public function getEntityClass()
+    {
+        return Export::class;
+    }
+
+    public function buildQuery(QueryBuilder $qb, array $query): void
+    {
+        $expr = $qb->expr();
+
+        if (isset($query['exporter_id'])) {
+            $qb->andWhere(
+                $expr->eq(
+                    'omeka_root.exporter',
+                    $this->createNamedParameter($qb, $query['exporter_id'])
+                )
+            );
+        }
+
+        if (isset($query['job_id'])) {
+            $qb->andWhere(
+                $expr->eq(
+                    'omeka_root.job',
+                    $this->createNamedParameter($qb, $query['job_id'])
+                )
+            );
+        }
+    }
+
+    public function hydrate(Request $request, EntityInterface $entity, ErrorStore $errorStore): void
+    {
+        $data = $request->getContent();
+        foreach ($data as $key => $value) {
+            $posColon = strpos($key, ':');
+            $keyName = $posColon === false ? $key : substr($key, $posColon + 1);
+            $method = 'set' . ucfirst(Inflector::camelize($keyName));
+            if (!method_exists($entity, $method)) {
+                continue;
+            }
+            $entity->$method($value);
+        }
+    }
+}
---
+++ modules/BulkExport/src/Service/ViewHelper/ListFormattersFactory.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+namespace BulkExport\Service\ViewHelper;
+
+use BulkExport\View\Helper\ListFormatters;
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class ListFormattersFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new ListFormatters(
+            $services->get('BulkExport\Formatter\Manager'),
+            $services->get('Config')['formatters']['aliases']
+        );
+    }
+}
